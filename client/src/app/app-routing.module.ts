0dffb2d62179f7ccdeccb432cf3f8ec5366fcc1b
Joseph Haworth <josephhaworth004@gmail.com>
2023-03-31 15:49:27 +0100

    End of Section 20

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -14,15 +14,19 @@ const routes: Routes = [
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule)},
   
-  {path: 'checkout',
-  canActivate: [AuthGuard],
+  {
+    path: 'checkout', 
+    canActivate: [AuthGuard],
     loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)
   },
-  
+  {
+    path: 'orders', 
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)
+  },
   {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
